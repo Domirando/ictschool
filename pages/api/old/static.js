b6cc6f8ccc347ae2a9775fa3433c8b2cd1488841
UwUssimo Robinson <uwussimo@icloud.com>
2022-02-26 18:36:13 +0500

    static url link updated

--- pages/api/old/static.js
+++ pages/api/old/static.js
@@ -1,6 +1,6 @@
 const staticApi = async (req, res) => {
   try {
-    res.redirect(307, "https://ictschool.uz/wp-json/");
+    res.redirect(307, "https://ictschool.uz/wp-json/wp/v2/");
   } catch (err) {
     res.status(500).send({ error: "failed to transfer to static" });
   }
