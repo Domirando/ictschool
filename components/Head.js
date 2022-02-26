26c051b794bb050bb70ca686c6dd06b908fd3c87
UwUssimo Robinson <uwussimo@icloud.com>
2022-02-26 21:09:05 +0500

    custom page constructor

---
+++ components/Head.js
@@ -0,0 +1,38 @@
+import NextHead from "next/head";
+
+const Head = ({
+  title = "Muhammad al-Xorazmiy AKT maktabi | ICTSchool.uz",
+  description = "Muhammad al-Xorazmiy nomidagi AKTga ixtisoslashtirilgan maktab | ICTSchool.uz",
+  image = "",
+  children,
+}) => {
+  return (
+    <NextHead>
+      {/* Title */}
+      <title>{title}</title>
+      <meta name="og:title" content={title} />
+
+      {/* Description */}
+      <meta name="description" content={description} />
+      <meta name="og:description" content={description} />
+
+      {/* Image */}
+      <meta name="twitter:image" content={image} />
+      <meta name="og:image" content={image} />
+
+      {/* URL */}
+      <meta name="og:url" content="https://ictschool.uz/" />
+
+      {/* General */}
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta httpEquiv="Content-Language" content="uz" />
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:site" content="@ictschool" />
+      <meta name="apple-mobile-web-app-title" content="ICTSchool" />
+      <meta name="author" content="Maftuna Vohidjonovna" />
+      {children}
+    </NextHead>
+  );
+};
+
+export default Head;
