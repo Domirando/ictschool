22bf9c18e29c2acf995e75a7ed166c8a27e6b398
Maftuna Vohidjonovna <maisiedev@gmail.com>
2022-03-03 06:10:47 +0500

    navbar styling

--- components/Navbar/index.js
+++ components/Navbar/index.js
@@ -1,19 +1,26 @@
 import styles from './navbar.module.css'
-import '@fortawesome/fontawesome-free/css/all.css';
+import {faEnvelopeOpen, faPhone} from '@fortawesome/free-solid-svg-icons';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
 
 const Navbar = () => {
     return (
         <nav>
-             {/*<img src="https://ictschool.uz/wp-content/uploads/2019/10/AKSS-logo3-UZ.png"/>*/}
+            {/*<img src="https://ictschool.uz/wp-content/uploads/2019/10/AKSS-logo3-UZ.png"/>*/}
             <div className={styles.topBarBg}>
                 <div className={styles.barContainer}>
                     <div className={styles.left}>
-                        <span className="flags">
-                            <img className="uzbFlag flag" src="https://ictschool.uz/wp-content/plugins/polylang/flags/uz.png" width="18" height="12"/>
-                        </span>
-                        <span className="flags">
-                            <img className="ruFlag flag" src="https://ictschool.uz/wp-content/plugins/polylang/flags/ru.png" width="18" height="12"/>
-                        </span>
+                        <img className={styles.flag}
+                             src="https://ictschool.uz/wp-content/plugins/polylang/flags/uz.png" width="18"
+                             height="12"/>
+                        <img className={styles.flag}
+                             src="https://ictschool.uz/wp-content/plugins/polylang/flags/ru.png" width="18"
+                             height="12"/>
+                        <FontAwesomeIcon className={styles.icon} icon={faEnvelopeOpen}
+                                         font-size={"15px"}
+                                         color="white"/>
+                        <p>info@ictschool.uz</p>
+                        <FontAwesomeIcon className={styles.icon} icon={faPhone} font-size={"15px"} color="white"/>
+                        <p className={styles.num}><a>1206</a></p>
                     </div>
                     <div className={styles.right}><p>E-CLASS</p></div>
                 </div>
