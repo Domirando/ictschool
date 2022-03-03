3e30f204fb1b5b02701843deeff448223523d1b9
Maftuna Vohidjonovna <maisiedev@gmail.com>
2022-03-03 05:16:11 +0500

    styling

--- components/Navbar/index.js
+++ components/Navbar/index.js
@@ -3,19 +3,19 @@ import '@fortawesome/fontawesome-free/css/all.css';
 
 const Navbar = () => {
     return (
-        <nav className={styles.nav}>
+        <nav>
              {/*<img src="https://ictschool.uz/wp-content/uploads/2019/10/AKSS-logo3-UZ.png"/>*/}
-            <div className="top-bar">
-                <div className="bar-container">
-                    <span className="top-bar-left">
+            <div className={styles.topBarBg}>
+                <div className={styles.barContainer}>
+                    <div className={styles.left}>
                         <span className="flags">
                             <img className="uzbFlag flag" src="https://ictschool.uz/wp-content/plugins/polylang/flags/uz.png" width="18" height="12"/>
                         </span>
                         <span className="flags">
                             <img className="ruFlag flag" src="https://ictschool.uz/wp-content/plugins/polylang/flags/ru.png" width="18" height="12"/>
                         </span>
-                    </span>
-                    <div className="top-bar-right">E-CLASS <i className="fa fa-envelope-open-o" aria-hidden="true"></i></div>
+                    </div>
+                    <div className={styles.right}><p>E-CLASS</p></div>
                 </div>
             </div>
         </nav>
