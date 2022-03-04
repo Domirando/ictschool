346a158464c35525085e4674988ddb77a7d3911b
Maftuna Vohidjonovna <maisiedev@gmail.com>
2022-03-04 22:03:08 +0500

    adding header

--- components/Header/index.js
+++ components/Header/index.js
@@ -1,17 +1,38 @@
 import styles from "./header.module.css";
-const Header = ({ title, text, gradient, btn, image }) => {
-  return (
-    <div>
-      {/*logo-image*/}
+import Image from 'next/image'
 
-      {/*nav*/}
+const Header = ({title, text, image, gradient, btn, headerBg, headerHeight}) => {
+    return (
+        <div className="headerContainer">
+            <style jsx>{`
+              .headerContainer {
+                background-image: url("${headerBg}");
+                min-height: ${headerHeight};
+                background-repeat: no-repeat;
+                background-size: cover;
+                background-position: center center;
+              }
+            `}</style>
+            {/*logo-image*/}
+            <div className={styles.logo}>
+                <Image
+                    src={
+                        "https://ictschool.uz/wp-content/uploads/2019/10/AKSS-logo3-UZ-300x31.png"
+                    }
+                    alt={"logo"}
+                    height="40px"
+                    width="300px"
+                />
+            </div>
 
-      {/*title*/}
+            {/*nav*/}
 
-      {/*text*/}
+            {/*title*/}
 
-      {/*btn*/}
-    </div>
-  );
+            {/*text*/}
+
+            {/*btn*/}
+        </div>
+    );
 };
 export default Header;
