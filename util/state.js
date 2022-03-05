0eaed06148772a9067db2b4e467ec1b8805392d7
Maftuna Vohidjonovna <maisiedev@gmail.com>
2022-03-06 04:54:02 +0500

    refactoring state

--- util/state.js
+++ util/state.js
@@ -2,88 +2,102 @@ let state = {
   navbar: [
     {
       title: "Bosh sahifa",
+      url: "/",
       items: null,
     },
     {
       title: "Maktab haqida",
+      url: "/smth",
       items: [
         {
           title: "Maktab haqida",
-          items: [
-            "Maktab hayoti",
-            "Maktab yutuqlari",
-            "Maktab logosi",
-            "Qonun hujjatlari",
+          url: "/smth",
+          items: [{
+            title: "Maktab hayoti", url: "/smth", items: null},
+            {title: "Maktab yutuqlari", url: "/smth", items: null},
+            {title: "Maktab logosi", url: "/smth", items: null},
+            {title: "Qonun hujjatlari", url: "/smth", items: null},
           ],
         },
         {
           title: "Tuzilma",
-          items: ["Rahbariyat", "Ilmiy kengash", "O'qituvchilar"],
+          url: "/smth",
+          items: [{title: "Rahbariyat", url: "/smth", items: null}, {title: "Ilmiy kengash", url: "/smth", items: null}, {title: "O'qituvchilar", url: "/smth", items: null}],
         },
         {
           title: "logo",
+          url: "/smth",
           items: null,
         },
       ],
     },
     {
       title: "Qabul",
+      url: "/smth",
       items: [
-        "Qabul haqida",
-        "Onlayn hujjat topshirish",
-        "2021/2022 o'quv yili qabul natijalari",
+        {title: "Qabul haqida", url: "/smth", items: null},
+        {title: "Onlayn hujjat topshirish", url: "/smth", items: null},
+        {title: "2021/2022 o'quv yili qabul natijalari", url: "/smth", items: null},
       ],
     },
     {
       title: "Yangiliklar",
+      url: "/smth",
       items: null,
     },
     {
       title: "Maktab hayoti",
+      url: "/smth",
       items: [
-        "E'lonlar",
-        "Tadbirlar",
-        "Olimpiadalar",
+        {title: "E'lonlar", url: "/smth", items: null},
+        {title: "Tadbirlar", url: "/smth", items: null},
+        {title: "Olimpiadalar", url: "/smth", items: null},
         {
           title: "Tanlov & Konkurslar",
+          url: "/smth",
           items: [
             {
               title: "Al-Xorazmiy izdoshlari",
-              items: ["Kirish"],
+              url: "/smth",
+              items: [{title: "Kirish", url: "/smth", items: null}],
             },
           ],
         },
         {
           title: "Loyihalar",
+          url: "/smth",
           items: [
-            "Digital Generation Uzbekistan",
-            "Olimpiada Assotsatsiyasi",
-            "Hour of Code",
+            {title: "Digital Generation Uzbekistan", url: "/smth", items: null},
+            {title: "Olimpiada Assotsatsiyasi", url: "/smth", items: null},
+            {title: "Hour of Code", url: "/smth", items: null},
           ],
         },
         {
           title: "Hamkorlar",
+          url: "/smth",
           items: [
-            "Huawei",
-            "Artel",
-            "DGUzbekistan",
-            "UzCard",
-            "Ucell",
-            "Hamkor bo'lish",
+            {title: "Huawei", url: "/smth", items: null},
+            {title: "Artel", url: "/smth", items: null},
+            {title: "DGUzbekistan", url: "/smth", items: null},
+            {title: "UzCard", url: "/smth", items: null},
+            {title: "Ucell", url: "/smth", items: null},
+            {title: "Hamkor bo'lish", url: "/smth", items: null},
           ],
         },
-        "Onlayn darslar",
-        "Ota-onalarga",
-        "O'quvchilar yotoqxonasi",
+        {title: "Onlayn darslar", url: "/smth", items: null},
+        {title: "Ota-onalarga", url: "/smth", items: null},
+        {title: "O'quvchilar yotoqxonasi", url: "/smth", items: null},
       ],
     },
     {
       title: "Onlayn darslar",
+      url: "/smth",
       items: null,
     },
     {
       title: "Bog'lanish",
-      items: ["Maktab bilan a'loqa"],
+      url: "/smth",
+      items: [{title: "Maktab bilan a'loqa", url: "/smth", items: null}],
     },
   ],
   footerColumnData: [
