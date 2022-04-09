972756073ee49b5b3f9d3215f59afb4c11942597
Maftuna Vohidjonovna <maisiedev@gmail.com>
2022-04-09 20:35:10 +0500

    fixed a bug on sidebar

--- components/SideNav/index.js
+++ components/SideNav/index.js
@@ -1,49 +1,68 @@
 import styles from "./styles.module.css";
 import { faAngleDown } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-
-function Item({ item, index, open, depth }) {
-  if (item.submenu && item.submenu.length > 0) {
-    return (
-      // FixMe: Onclick is not working properly...
-      <li
-        key={index}
-        className={
-          "w-full flex justify-between items-center text-green-400 hover:cursor-pointer"
-        }
-        onClick={() => (
-          <SideNav state={item.submenu} open={true} depth={depth++} />
-        )}
-      >
-        {item.title}
-        <FontAwesomeIcon
-          icon={faAngleDown}
-          className={depth == 1 ? null : "rotate-[-90deg]"}
-        />
-      </li>
-    );
-  } else {
-    return (
-      <li key={index} className={"text-red-400"}>
-        {item.title}
-      </li>
-    );
-  }
-}
-
+import { useState } from "react";
 const SideNav = ({ open = false, state = ["no data on menu"], depth = 0 }) => {
+  let [data, setData] = useState(false);
+  function Item({ item, index, open, depth }) {
+    if (item.submenu && item.submenu.length > 0) {
+      return (
+        // FixMe: Onclick is not working properly...
+        <li
+          key={index}
+          className={
+            "w-full flex justify-between items-center hover:cursor-pointer"
+          }
+          onClick={() => setData(item.submenu)}
+        >
+          {item.title}
+          <FontAwesomeIcon
+            icon={faAngleDown}
+            className={depth == 1 ? null : "rotate-[-90deg]"}
+          />
+        </li>
+      );
+    } else {
+      return <li key={index}>{item.title}</li>;
+    }
+  }
   return (
-    <ul
-      className={
-        open
-          ? `flex ${depth ? "bg-red-400" : styles.side_nav}`
-          : `hidden ${styles.side_nav}`
-      }
-    >
-      {state.map((item, index) => (
-        <Item item={item} key={index} index={index} open={open} depth={depth} />
-      ))}
-    </ul>
+    <>
+      {" "}
+      {data ? (
+        <ul
+          className={
+            open ? `flex ${styles.side_nav}` : `hidden ${styles.side_nav}`
+          }
+        >
+          {data.map((item, index) => (
+            <Item
+              item={item}
+              key={index}
+              index={index}
+              open={open}
+              depth={depth}
+            />
+          ))}
+        </ul>
+      ) : (
+        <ul
+          className={
+            open ? `flex ${styles.side_nav}` : `hidden ${styles.side_nav}`
+          }
+        >
+          {state.map((item, index) => (
+            <Item
+              item={item}
+              key={index}
+              index={index}
+              open={open}
+              depth={depth}
+            />
+          ))}
+        </ul>
+      )}
+    </>
   );
 };
 
