53d82fb948331550952f3bb22484c060a848eaed
Maftuna Vohidjonovna <maisiedev@gmail.com>
2022-04-11 20:11:42 +0500

    changed

--- components/Error/index.js
+++ components/Error/index.js
@@ -16,24 +16,49 @@ function Error(props) {
           justify-content: center;
           align-items: center;
           margin-top: -5vh;
-          margin-left: 80px;
         }
+
         .container > h1 {
           font-size: 150px;
           font-weight: bold;
         }
+
         .container > h2 {
           font-size: 35px;
           font-weight: bold;
           margin-bottom: 5px;
         }
+
         .container > h3 {
           font-size: 18px;
           margin-bottom: 50px;
         }
+
         .container > button {
           font-size: 17px;
           font-weight: bold;
+        }
+
+        @media (max-width: 760px) {
+          .container {
+            margin-top: 8vh;
+          }
+
+          .container > h1 {
+            font-size: 60px;
+            font-weight: bold;
+          }
+
+          .container > h2 {
+            font-size: 30px;
+            font-weight: bold;
+            margin-bottom: 5px;
+          }
+
+          .container > h3 {
+            font-size: 15px;
+            margin-bottom: 50px;
+          }
         }
       `}</style>
     </div>
