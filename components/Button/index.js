897ec132403e1d6fe7ded1b45f858dc28c1d5a10
Maftuna Vohidjonovna Domirando <maisiedev.gmail.com>
2023-07-12 10:58:21 +0500

    changed

--- components/Button/index.js
+++ components/Button/index.js
@@ -9,6 +9,7 @@ const Button = ({
   py = "py-2",
   href = "/404",
 }) => {
+  // return
   return (
     <button
       className={`${border} ${bgcolor} ${py} ${fontSize} ${width} px-8  flex items-center justify-center font-bold text-white ${
